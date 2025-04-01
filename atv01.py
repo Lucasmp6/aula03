205eec7e7daf7b12fa7fa4ad8a4651eecfd6078a
Lucas <Lucas lucasmp636@gmail.com>
2025-04-01 08:51:11 -0300

    Adicionei na pasta aula03 o nome do usuario, a idade e o seu salario

---
+++ atv01.py
@@ -0,0 +1,4 @@
+nome = input(("Qual é o seu nome: "))
+idade = int(input("Qual a sua idade: "))
+salario = float(input("Qual o seu salario: "))
+print (f"Olá {nome} sua idade é {idade} e o seu salario é {salario} reais :")
