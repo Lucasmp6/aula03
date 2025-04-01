d56dea1ac25848e9dbb8699f9a7cc5bda537d2f6
Lucas <Lucas lucasmp636@gmail.com>
2025-04-01 09:47:55 -0300

    Adicionei todas as operacoes e todos os resultados da adicao,multiplicacao,subtracao e divisao

---
+++ exercicio02.py
@@ -0,0 +1,11 @@
+numero1 = float(input("Digite o numero: "))
+numero2 = float(input("Digite o segundo numero: "))
+adicao = numero1+numero2
+subtracao = numero1-numero2
+multiplicacao = numero1*numero2
+divisao = numero1/numero2
+
+print(f"{numero1} + {numero2} = {adicao} ")
+print(f"{numero1} - {numero2} = {subtracao} ")
+print(f"{numero1} * {numero2} = {multiplicacao} ")
+print(f"{numero1} / {numero2} = {divisao} ")
