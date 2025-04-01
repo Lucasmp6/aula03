b655395e79afe01b9f35da0e6ea3ff5923acebb5
Lucas <Lucas lucasmp636@gmail.com>
2025-04-01 09:21:58 -0300

    Adicionei um calculo de percentual no aumento do salario dele

--- atv01.py
+++ atv01.py
@@ -1,4 +1,8 @@
 nome = input(("Qual é o seu nome: "))
 idade = int(input("Qual a sua idade: "))
 salario = float(input("Qual o seu salario: "))
-print (f"Olá {nome} sua idade é {idade} e o seu salario é {salario} reais :")
+aumento = float(input("Qual foi o percentual de aumento que voce teve: "))
+valorreal = salario*aumento/100
+novosalario = salario+valorreal
+print (f"Olá {nome} sua idade é {idade} e o seu salario atual é {salario} reais :")
+print (f"seu aumento é {novosalario} ")
