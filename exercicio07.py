f2f8372575b0b8a506a55ea11e57051c417ce169
Lucas <Lucas lucasmp636@gmail.com>
2025-04-03 09:16:15 -0300

    alterei um pouco o codigo do exercicio7 e adicionei umas coisinhas a mais

--- exercicio07.py
+++ exercicio07.py
@@ -1,3 +1,7 @@
+print("Seja bem Vindo ao Posto ao Minecraft")
+print(" ")
+print("Aqui o cliente sempre tem vantagem")
+print(" ")
 gasolina = 5.80
 etanol = 4.90
 
@@ -8,7 +12,10 @@ valorPagamentoEtanol = etanol * valorSelecionado
 valorPagamentoGasolina =  gasolina * valorSelecionado
 
 if combustivelSelecionado == "G":
-    print(valorPagamentoGasolina)
+    print(f"Essa é a quantidade de valor que vc terá que pagar: R${valorPagamentoGasolina}")
 elif combustivelSelecionado == "E":
-    print(valorPagamentoEtanol)
-
+    print(f"Essa é a quantidade de valor que vc terá que pagar: R${valorPagamentoEtanol:.2f}")
+else:
+    print("Não estamos entendendo qual combustivel vc quer!")
+print(" ")
+print("Agradecemos a sua Preferencia")
